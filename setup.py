81442606ef8adfb33f1b0e754d6f4b8adc5adc4a
Guo Zhang <zhangguo@quanttide.com>
2020-07-11 17:39:57 +0800

    create

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='wechatwork-sdk-py',
+    version='0.0.1',
+    packages=['wechatwork_sdk'],
+    url='quanttide.com',
+    license='MIT',
+    author='QuantTide Inc.',
+    author_email='zhangguo@quanttide.com',
+    description='企业微信第三方SDK-Python版本'
+)
