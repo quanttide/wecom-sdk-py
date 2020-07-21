afa3255b6728160c8e2874b09f6f53bbb7eab42c
Guo Zhang <zhangguo@quanttide.com>
2020-07-21 19:10:56 +0800

    修改API文档样例

--- wechatwork_sdk/contact.py
+++ wechatwork_sdk/contact.py
@@ -8,20 +8,19 @@ class UserSDK(WeChatWorkSDK):
         super().__init__(corpid, contact_secret)
         self._api_root_url = self._api_root_url + 'user/'
 
-    def create(self):
-        return self.post_api(api='create')
+    def create(self, userid, name, **kwargs):
+        pass
 
     def get(self, userid):
+        """
+        读取成员
+        :param userid: 成员UserID。对应管理端的帐号，企业内必须唯一。不区分大小写，长度为1~64个字节。
+        :return:
+        """
         return self.get_api(api='get', query_params={'userid': userid})
-
-    def update(self):
-        return self.post_api(api='update')
 
 
 class DepartmentSDK(WeChatWorkSDK):
     def __init__(self, corpid, contact_secret):
         super().__init__(corpid, contact_secret)
         self._api_root_url = self._api_root_url + 'department/'
-
-    def create(self):
-        pass
