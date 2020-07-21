80b05ead59575cae5bb34e7adc4d0471d21620a9
Guo Zhang <zhangguo@quanttide.com>
2020-07-21 19:06:38 +0800

    修改测试样例

--- tests/test_base.py
+++ tests/test_base.py
@@ -19,6 +19,15 @@ class WeChatWorkSDKTestCase(unittest.TestCase):
         return_data = self.wechatwork_sdk.request_api('GET', 'user/simplelist', {'department_id': 1})
         self.assertTrue('userlist' in return_data)
 
+    def test_refresh_access_token(self):
+        """
+        刷新access_token的逻辑内置在request_api方法中，检测42001错误码删除缓存
+        企业微信服务器在生成access_token以后会缓存，验证30分钟过期以后的处理十分困难
+        TODO: 实现刷新access_token逻辑的测试
+        :return:
+        """
+        self.assertTrue(True, False)
+
 
 if __name__ == '__main__':
     unittest.main()
