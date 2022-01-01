bcd24b5d7188119d6ccafd7a1751ba931aa68a41
Guo Zhang <zhangguo@quanttide.com>
2022-01-01 17:49:59 +0800

    配置审查

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from distutils.core import setup
 
-setup(
-    name='wecom-sdk-py',
-    version='0.0.1',
-    packages=['wecom_sdk'],
-    url='quanttide.com',
-    license='Apache 2.0',
-    author='QuantTide Inc.',
-    author_email='tech@quanttide.com',
-    description='企业微信Python服务端SDK'
-)
+setup()
